e67f94294739a4355e3b3103849f198762bb722f
Esemega <sogaral89@gmail.com>
2020-04-25 19:14:00 +0200

    creating the structure of the API

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
+
 const database = {
     users:[
         {
@@ -24,7 +24,8 @@ const database = {
     ]
 }
 app.get('/', (req, res) => {
-    res.send('This is working!')
+    // res.send('This is working!');
+    res.json(database.users);
 })
 
 app.post('/signin', (req,res) =>{
@@ -32,6 +33,52 @@ app.post('/signin', (req,res) =>{
         res.json('success')
     } else {
         res.status(400).json('error logging in')
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, password, name } = req.body;
+
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+
+    res.json(database.users[database.users.length -1]);
+})
+
+app.get('/profile/:id', (req,res) => {
+    const { id } = req.params;
+    let found = false;
+
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(!found) {
+        res.status(400).json('user not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if(!found) {
+        res.status(400).json('user not found');
     }
 })
 
