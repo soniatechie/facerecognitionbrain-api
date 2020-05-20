20e21202b8ab6c366eb01515102bf7bd52918c78
Esemega <sogaral89@gmail.com>
2020-05-20 18:43:11 +0200

    connecting profile with database

--- server.js
+++ server.js
@@ -81,17 +81,16 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req,res) => {
     const { id } = req.params;
-    let found = false;
-
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            return res.json(user);
+    
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if(user.length){
+            res.json(user[0]);
+        } else {
+            res.status(400).json('User not found')
         }
     })
-    if(!found) {
-        res.status(400).json('user not found');
-    }
+    .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
