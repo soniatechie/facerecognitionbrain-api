b0f471ff18009a3f895bcac577f19f3d5ef41b14
Esemega <sogaral89@gmail.com>
2020-05-28 19:29:21 +0200

    changing response to root

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/', (req, res) => {res.json(database.users)})
+app.get('/', (req, res) => {res.send('it is working!')})
 
 app.post('/signin', (req, res) => signin.handlerSignin(req, res, db, bcrypt))
 app.post('/register', (req, res) => register.handlerRegister(req, res, db, bcrypt))
