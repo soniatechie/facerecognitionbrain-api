df0803692aafa9e218addf307bcf5e0ed9719bc5
Esemega <sogaral89@gmail.com>
2020-05-29 20:13:22 +0200

    fixing database connection

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ const register = require('./controllers/register');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+// postgresql-spherical-05454
 
 const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
+        ssl: true,
     }
 });
 
