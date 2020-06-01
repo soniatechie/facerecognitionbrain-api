478cc90e69dd7e6a3f6dccbeb1a690d539b4716c
Esemega <sogaral89@gmail.com>
2020-06-01 19:11:02 +0200

    setting NODE_TLS_REJECTED_UNAUTHORIZED to 0

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const register = require('./controllers/register');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 // postgresql-spherical-05454
 
 const db = knex({
